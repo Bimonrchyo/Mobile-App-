58003a56666b595bafc37431ce7222a49f655d28
Bimonrchyo <mohammad.bimo.nurcahyo@gmail.com>
2025-11-15 15:31:16 +0700

    tambah logika back, Page 3 Upd 4

--- app/src/main/java/com/example/smkn1app/MainActivity3.kt
+++ app/src/main/java/com/example/smkn1app/MainActivity3.kt
@@ -1,11 +1,20 @@
 package com.example.smkn1app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity3: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+
+        val back =findViewById<Button>(R.id.back)
+
+        back.setOnClickListener {
+            val intent = Intent (this, MainActivity2 ::class.java)
+            startActivity(intent)
     }
+  }
 }
