bfab74be798f53f1af063e5f502e8854fd5639c3
Bimonrchyo <mohammad.bimo.nurcahyo@gmail.com>
2025-11-15 12:24:33 +0700

    kasih logika button ganti page, Page 1

--- app/src/main/java/com/example/smkn1app/MainActivity2.kt
+++ app/src/main/java/com/example/smkn1app/MainActivity2.kt
@@ -1,4 +1,13 @@
 package com.example.smkn1app
 
-class MainActivity2 {
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity2: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
 }
