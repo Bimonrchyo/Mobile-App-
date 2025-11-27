c50b0d67452816194c1733000677c92a5fda67bc
zaidanehnf <edundtrizy@gmail.com>
2025-11-27 10:35:25 +0700

    Initial commit SMKN1App2

---
+++ app/src/main/java/com/smkn1/smkn1app/AboutActivity.kt
@@ -0,0 +1,35 @@
+package com.smkn1.app
+
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class AboutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+
+        val navHome = findViewById<ImageView>(R.id.nav_home)
+        val navBook = findViewById<ImageView>(R.id.nav_book)
+        val navProfile = findViewById<ImageView>(R.id.nav_profile)
+
+        // ICON AKTIF = BUKU
+        setActiveFooter()
+
+        // footer tidak pindah halaman
+        navHome.setOnClickListener {}
+        navBook.setOnClickListener {}
+        navProfile.setOnClickListener {}
+    }
+
+    private fun setActiveFooter() {
+        val navBook = findViewById<ImageView>(R.id.nav_book)
+
+        val dark = PorterDuffColorFilter(0x80000000.toInt(), PorterDuff.Mode.SRC_ATOP)
+
+        navBook.colorFilter = dark     // ikon buku gelap
+    }
+}
